6f9fb31edd127ed5baf55e0e786fbd76c95ffc1c
jykim1428 <jykim1428@hufs.ac.kr>
2024-07-28 21:58:52 +0900

    inital setting

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+#from django.contrib.auth.models import User
+
+# Create your models here.
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bio = models.TextField()
+#     location = models.CharField(max_length=30)
+#     birth_date = models.DateField(null=True, blank=True)
