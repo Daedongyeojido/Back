7dcd45ee71d7a0f428ccee45c500e8405161d89d
jykim1428 <jykim1428@hufs.ac.kr>
2024-07-30 21:59:19 +0900

    login join end

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
+# users/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import MyUser
 
-# Register your models here.
+class MyUserAdmin(UserAdmin):
+    model = MyUser
+    list_display = ['email', 'nickname', 'is_active', 'is_staff']
+    fieldsets = (
+        (None, {'fields': ('email', 'nickname', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'nickname', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email', 'nickname')
+    ordering = ('email',)
+
+admin.site.register(MyUser, MyUserAdmin)
