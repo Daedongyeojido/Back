023f82411cbf13ddeb5c7bbd31d3e6b5b61869c6
yanghyen <sk0310803@naver.com>
2024-07-31 16:11:36 +0900

    ADD: 모델 및 초기 세팅

---
+++ route/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Route(models.Model):
+  route_id = models.AutoField(primary_key=True)
+  startpoint_name = models.CharField(max_length=50)
+  startpoint_address = models.CharField(max_length=50)
+  startpoint_x = models.IntegerField()
+  startpoint_y = models.IntegerField()
+  endpoint_name = models.CharField(max_length=50)
+  endpoint_address = models.CharField(max_length=50)
+  endpoint_x = models.IntegerField()
+  endpoint_y = models.IntegerField()
+  date = models.DateField()
+  user_id = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+class Place(models.Model):
+  place_id = models.AutoField(primary_key=True)
+  route_id = models.ForeignKey(Route, on_delete=models.CASCADE)
+  place_name = models.CharField(max_length=50)
+  place_address = models.CharField(max_length=50)
+  place_like = models.IntegerField()
+  place_x = models.IntegerField()
+  place_y = models.IntegerField()
+  category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+  subcategory_id = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+
+class Route_places(models.Model):
+  route_places_id = models.AutoField(primary_key=True)
+  route_id = models.ForeignKey(Route, on_delete=models.CASCADE)
+  user_like = models.BooleanField()
